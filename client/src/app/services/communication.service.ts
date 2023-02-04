225b59cd7afb5f499d25c8a76ecfa8ad538d1622
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-04 17:14:57 -0500

    added generic methods for http communications

--- client/src/app/services/communication.service.ts
+++ client/src/app/services/communication.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,10 +8,23 @@ import { Injectable } from '@angular/core';
 export class CommunicationService {
   private readonly BASE_URL: string = '';
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) { }
 
-  async registerUser(){
-    
+  async get<T>(route: string){
+    return await this.observableToPromise(this.http.get(`${this.BASE_URL}/${route}`));
+  }
+
+  async post<T, R>(body: T, route: string){
+    return await this.observableToPromise<R>(this.http.post<R>(`${this.BASE_URL}/${route}`, body));
+  }
+
+  private async observableToPromise<T>(observable: Observable<T>): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      observable.subscribe({
+        next: (value: T) => resolve(value),
+        error: (error) => reject(error)
+      });
+    });
   }
 
 }

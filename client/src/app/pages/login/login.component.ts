351a7ecc9d2cf59bf794f8b8f0d7675efd0f5b24
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-04 18:14:50 -0500

    added formcontrols to input

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { CommunicationService } from 'src/app/services/communication.service';
+
+const NUM_FIELD_FORM = 2;
 
 @Component({
   selector: 'app-login',
@@ -6,15 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public form: FormControl[] = [];
 
-  constructor() { }
+  constructor(communicationService: CommunicationService) {
+    this.form[0] = new FormControl("");
+    this.form[1] = new FormControl("");
+  }
 
   ngOnInit(): void {
 
   }
 
   async login() {
+    console.log(this.form[0].value);
+    console.log(this.form[1].value);
+    this.clearForm();
+  }
 
+  private clearForm(): void {
+    for(let i = 0; i < NUM_FIELD_FORM; i++) this.form[i].reset();
   }
 
 }

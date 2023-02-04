d07dd0e18c680efbe8884970afdbb4c59518f91d
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-04 18:15:12 -0500

    basic main user profile page

--- client/src/app/pages/user/user.component.ts
+++ client/src/app/pages/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserManagerService } from 'src/app/services/user-manager.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(public userManager: UserManagerService) { }
 
   ngOnInit(): void {
+
   }
 
 }

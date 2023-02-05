d89ece0ff9126d181ed4f0f0d47f0f5469df2269
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-05 05:06:37 -0500

    made header into a separate component for reuse

--- client/src/app/components/secondary-header/secondary-header.component.ts
+++ client/src/app/components/secondary-header/secondary-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserManagerService } from 'src/app/services/user-manager.service';
 
 @Component({
   selector: 'app-secondary-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondaryHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public userManager: UserManagerService) { }
 
   ngOnInit(): void {
   }

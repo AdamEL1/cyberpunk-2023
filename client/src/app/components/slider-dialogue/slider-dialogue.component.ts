75aea6ecf1ee1fe29658f40f5dd0d0b833a6a8cc
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-05 08:09:37 -0500

    added slider component

---
+++ client/src/app/components/slider-dialogue/slider-dialogue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-dialogue',
+  templateUrl: './slider-dialogue.component.html',
+  styleUrls: ['./slider-dialogue.component.css']
+})
+export class SliderDialogueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

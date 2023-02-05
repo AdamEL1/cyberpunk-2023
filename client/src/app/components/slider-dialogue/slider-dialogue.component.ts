543779e82089e63adc2414dbfbd2d2feda26ad9e
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-05 09:04:03 -0500

    made slider dialog functionnal

--- client/src/app/components/slider-dialogue/slider-dialogue.component.ts
+++ client/src/app/components/slider-dialogue/slider-dialogue.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+
+const NUM_FIELDS = 6;
 
 @Component({
   selector: 'app-slider-dialogue',
   templateUrl: './slider-dialogue.component.html',
   styleUrls: ['./slider-dialogue.component.css']
 })
-export class SliderDialogueComponent implements OnInit {
+export class SliderDialogueComponent {
+  public formControls: FormControl[] = [];
 
-  constructor() { }
+  constructor(private dialogueRef: MatDialogRef<SliderDialogueComponent>) { 
+    for(let i = 0; i < NUM_FIELDS; i++) this.formControls[i] = new FormControl(5);
+  }
 
-  ngOnInit(): void {
+  submitDialog(){
+    const values: number[] = [];
+    for(let i = 0; i < NUM_FIELDS; i++) values.push(this.formControls[i].value);
+    this.dialogueRef.close({data: values});
   }
 
 }

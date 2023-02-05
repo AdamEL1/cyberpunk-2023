bfd4b74fbfe13f3214ed766ed37fcaf518d3a02c
lemark <marcantoine_manningham@outlook.com>
2023-02-05 10:22:37 -0500

    Email

--- client/src/app/pages/suggested-users/suggested-users.component.ts
+++ client/src/app/pages/suggested-users/suggested-users.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserManagerService } from 'src/app/services/user-manager.service';
 
+
 @Component({
   selector: 'app-suggested-users',
   templateUrl: './suggested-users.component.html',
   styleUrls: ['./suggested-users.component.css']
 })
 export class SuggestedUsersComponent {
-
+  public email: string;
   constructor(public userManager: UserManagerService, private router: Router) { 
     if(!this.userManager.getUser()) router.navigateByUrl("");
+    this.email = "mailto:" + this.userManager.getUser()!.email;
   }
-
 }
+

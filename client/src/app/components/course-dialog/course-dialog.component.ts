a92de2ba811419d9561375a431e17464a213aa09
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-05 04:39:40 -0500

    removed useless oninit method

--- client/src/app/components/course-dialog/course-dialog.component.ts
+++ client/src/app/components/course-dialog/course-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Course } from 'src/app/classes/course';
 import { ADD_COURSE_ROUTE } from 'src/app/constants';
 import { CommunicationService } from 'src/app/services/communication.service';
@@ -10,14 +10,11 @@ import { UserManagerService } from 'src/app/services/user-manager.service';
   templateUrl: './course-dialog.component.html',
   styleUrls: ['./course-dialog.component.css']
 })
-export class CourseDialogComponent implements OnInit {
+export class CourseDialogComponent {
   public courseInput: FormControl;
   
-  constructor(private communicationService: CommunicationService, private userManager: UserManagerService) {
+  constructor(private communicationService: CommunicationService, private userManager: UserManagerService, private dialogRef: MatDialogRef<CourseDialogComponent>) {
     this.courseInput = new FormControl('');
-  }
-
-  ngOnInit(): void {
   }
 
   async addCourse(){
@@ -33,6 +30,7 @@ export class CourseDialogComponent implements OnInit {
     console.log(joinData);
     const state: {state: boolean} = await this.communicationService.post<CourseJoinData, {state: boolean}>(joinData, ADD_COURSE_ROUTE);
     this.courseInput.reset();
+
   }
 
   isValid(): boolean{

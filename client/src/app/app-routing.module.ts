c2589e466f6b9261da4ab13faf30e700f205f575
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-05 05:07:19 -0500

    used new header components in old pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { SuggestedUsersComponent } from './pages/suggested-users/suggested-users.component';
 import { UserComponent } from './pages/user/user.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'user', component: UserComponent},
+  {path: 'suggested-users', component: SuggestedUsersComponent},
   {path: "**", component: HomeComponent},
 ];
 

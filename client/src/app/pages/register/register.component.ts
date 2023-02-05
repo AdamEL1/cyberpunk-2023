f22a30334af83b79bb24f10f397c518377df3a64
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-04 23:13:59 -0500

    constructing user object from field data

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,7 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Course } from 'src/app/classes/course';
+import { User } from 'src/app/classes/user';
+import { CommunicationService } from 'src/app/services/communication.service';
 
-const NUM_FIELD_FORM = 4;
+const NUM_FIELD_FORM = 5;
+
+export enum FILED_INDEX {
+  NAME_FIELD_INDEX = 0,
+  PASSWORD_FIELD_INDEX = 1,
+  SCHOOL_FIELD_INDEX = 2,
+  EMAIL_FIELD_INDEX = 3,
+  COURSES_FIELD_INDEX = 4
+};
 
 @Component({
   selector: 'app-register',
@@ -11,16 +22,27 @@ const NUM_FIELD_FORM = 4;
 export class RegisterComponent implements OnInit {
   public form: FormControl[] = [];
 
-  constructor() { 
+  constructor(private communicationService: CommunicationService) { 
     for(let i = 0; i < NUM_FIELD_FORM; i++) this.form[i] = new FormControl("");
   }
 
   ngOnInit(): void {
-    
+
   }
 
   async register() {
-    for(let i = 0; i < NUM_FIELD_FORM; i++) console.log(this.form[i].value);
+    const user: User = {
+      name: this.form[FILED_INDEX.NAME_FIELD_INDEX].value,
+      password: this.form[FILED_INDEX.PASSWORD_FIELD_INDEX].value,
+      email: this.form[FILED_INDEX.EMAIL_FIELD_INDEX].value,
+      school: this.form[FILED_INDEX.SCHOOL_FIELD_INDEX].value,
+      courses: (this.form[FILED_INDEX.COURSES_FIELD_INDEX].value as string).split(',').map((value: string): Course => {
+        return {title: value};
+      }),
+      Description: ''
+    };
+    console.log(user);
+    await this.communicationService.post();
     this.clearForm();
   }
 

b4a82749bef702d8a211a9096c48f38a31fa4091
Willy Chea <heng-willy.chea@polymtl.ca>
2023-02-04 22:12:54 -0500

    WC: register same format as login

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+const NUM_FIELD_FORM = 3;
 
 @Component({
   selector: 'app-register',
@@ -6,15 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public form: FormControl[] = [];
 
-  constructor() { }
+  constructor() { 
+    this.form[0] = new FormControl("");
+    this.form[1] = new FormControl("");
+    this.form[2] = new FormControl("");
+  }
 
   ngOnInit(): void {
   }
 
-  async register(){
-
-    
+  async register() {
+    console.log(this.form[0].value);
+    console.log(this.form[1].value);
+    this.clearForm();
   }
+
+  private clearForm(): void {
+    for(let i = 0; i < NUM_FIELD_FORM; i++) this.form[i].reset();
+  }
+
+
+
+
 
 }

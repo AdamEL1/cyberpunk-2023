0f27569e09d4585f6ca28ed9b2733d940e3a4f83
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-05 04:54:48 -0500

    added suggested users page

---
+++ client/src/app/pages/suggested-users/suggested-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-suggested-users',
+  templateUrl: './suggested-users.component.html',
+  styleUrls: ['./suggested-users.component.css']
+})
+export class SuggestedUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

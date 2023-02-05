1e479ca351c9a806a23c1b6d4434a3aabee429cc
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-05 10:41:30 -0500

    add builded format

---
+++ client/src/app/classes/description.ts
@@ -0,0 +1,8 @@
+export const getMockedDescription = ():Description => {
+    return {    
+        creativity: 0,
+        punctuality: 0,
+        responsability: 0,
+        organized: 0
+    };
+};

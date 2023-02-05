f73c044cdd94d23815a08be44be896c3ddb1846f
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-04 23:47:01 -0500

    added routes consts

--- client/src/app/constants.ts
+++ client/src/app/constants.ts
@@ -1,4 +1,4 @@
 export const REGISTER_USER_ROUTE = 'users/register';
-export const LOGIN_ROUTE = 'courses/login';
-export const REGISTER_COURSE_ROUTE = 'course/register';
+export const LOGIN_ROUTE = 'users/login';
+export const REGISTER_COURSE_ROUTE = 'courses/register';
 

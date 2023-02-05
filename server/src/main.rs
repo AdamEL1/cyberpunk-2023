7b4384500717397494079724f5496431d31de0a2
lemark <marcantoine_manningham@outlook.com>
2023-02-05 05:18:39 -0500

    Small change to CORS

--- server/src/main.rs
+++ server/src/main.rs
@@ -31,7 +31,7 @@ impl AppState {
 async fn main() -> tide::Result<()> {
     let cors = CorsMiddleware::new()
         .allow_methods("GET, POST".parse::<HeaderValue>().unwrap())
-        .allow_origin(Origin::from("*"))
+        .allow_origin(Origin::Any)
         .allow_credentials(false);
     let state = AppState::new();
     let mut app = tide::with_state(state);

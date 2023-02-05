d552ed52ba669758f8f8b61b3ee990b067630642
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-05 10:32:16 -0500

    updated email sending

--- client/src/app/pages/suggested-users/suggested-users.component.ts
+++ client/src/app/pages/suggested-users/suggested-users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/classes/user';
 import { UserManagerService } from 'src/app/services/user-manager.service';
 
 
@@ -9,10 +10,13 @@ import { UserManagerService } from 'src/app/services/user-manager.service';
   styleUrls: ['./suggested-users.component.css']
 })
 export class SuggestedUsersComponent {
-  public email: string;
+  public email: string = '';
   constructor(public userManager: UserManagerService, private router: Router) { 
     if(!this.userManager.getUser()) router.navigateByUrl("");
-    this.email = "mailto:" + this.userManager.getUser()!.email;
+  }
+
+  sendEmail(selectedUser: User){
+    this.email = "mailto:" + selectedUser.email;
   }
 }
 

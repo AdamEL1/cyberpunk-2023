8bcd831989352ebb1bb7444388927a99786d5e48
AdamEL1 <70984414+AdamEL1@users.noreply.github.com>
2023-02-04 22:43:37 -0500

    regitser component refarctor

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
-const NUM_FIELD_FORM = 3;
+const NUM_FIELD_FORM = 4;
 
 @Component({
   selector: 'app-register',
@@ -12,26 +12,20 @@ export class RegisterComponent implements OnInit {
   public form: FormControl[] = [];
 
   constructor() { 
-    this.form[0] = new FormControl("");
-    this.form[1] = new FormControl("");
-    this.form[2] = new FormControl("");
+    for(let i = 0; i < NUM_FIELD_FORM; i++) this.form[i] = new FormControl("");
   }
 
   ngOnInit(): void {
+    
   }
 
   async register() {
-    console.log(this.form[0].value);
-    console.log(this.form[1].value);
+    for(let i = 0; i < NUM_FIELD_FORM; i++) console.log(this.form[i].value);
     this.clearForm();
   }
 
   private clearForm(): void {
     for(let i = 0; i < NUM_FIELD_FORM; i++) this.form[i].reset();
   }
-
-
-
-
 
 }

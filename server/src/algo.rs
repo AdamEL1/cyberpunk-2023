5d294d527742952b3e57a1e027004bd4dc372044
lemark <marcantoine_manningham@outlook.com>
2023-02-05 09:57:01 -0500

    Important last minute fixes to the server

--- server/src/algo.rs
+++ server/src/algo.rs
@@ -7,25 +7,25 @@ use crate::{
 
 pub fn find_matches(user: User, other_users: Vec<User>) -> WeightedUserList {
     let mut users_match = vec![];
-    let self_desc = user.description.to_array();
-    let mean_user: isize = user.description.to_array().iter().sum();
+    let self_desc = user.description;
+    let mean_user: isize = user.description.iter().sum();
     for other_user in other_users {
         let user_score: isize = other_user
             .description
-            .to_array()
             .iter()
             .zip(self_desc.iter())
             .map(|(x, y)| cmp::max(x, y))
             .sum();
-        let mean_other_user: isize = other_user.description.to_array().iter().sum();
+        let mean_other_user: isize = other_user.description.iter().sum();
 
         let weight = (user_score - (mean_user - mean_other_user).abs()) as f64
-            / user.description.to_array().len() as f64;
+            / user.description.len() as f64;
         let weighted_user = WeightedUser {
             user: other_user.clone(),
             weight,
         };
         users_match.push(weighted_user);
     }
+    users_match.sort_by(|a, b| a.weight.partial_cmp(&b.weight).unwrap());
     WeightedUserList { data: users_match }
 }
